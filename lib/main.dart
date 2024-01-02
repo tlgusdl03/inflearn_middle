887e065fb5923bddf46bc3927228284e87105644
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-02 19:31:30 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:inputform_proj/common/component/custom_text_form.dart';
+
+void main() {
+  runApp(
+    _App(),
+  );
+}
+
+class _App extends StatelessWidget {
+  const _App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSans',
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요.',
+              onChanged: (String value){} ,
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요.',
+              onChanged: (String value){} ,
+              obscureText: true,
+            ),
+          ],
+        )
+      ),
+    );
+  }
+}
