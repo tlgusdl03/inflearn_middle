25b76e365b99ed844c7165ed2278a07a830c5c93
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-02 21:16:38 +0900

    로그인 UI 구성완료

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:inputform_proj/common/component/custom_text_form.dart';
+import 'package:inputform_proj/common/const/colors.dart';
 import 'package:inputform_proj/common/layout/default_layout.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -8,19 +9,84 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CustomTextFormField(
-            hintText: '이메일을 입력해주세요.',
-            onChanged: (String value) {},
+      child: SingleChildScrollView(
+        //화면을 스크롤 가능하게 만드는 위젯
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        //화면을 스크롤하면 키보드가 내려가게 함
+        child: SafeArea(
+          //안짤리고 화면 안에 들어오게 하는 위젯
+          top: true,
+          bottom: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _Title(),
+                const SizedBox(height: 16.0), // const SizedBox(height: 8.0
+                _SubTitle(),
+                Image.asset(
+                  'asset/img/misc/logo.png',
+                  width: MediaQuery.of(context).size.width / 3 * 2,
+                ),
+                CustomTextFormField(
+                  hintText: '이메일을 입력해주세요.',
+                  onChanged: (String value) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  hintText: '비밀번호를 입력해주세요.',
+                  onChanged: (String value) {},
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    primary: PRIMARY_COLOR,
+                  ),
+                  child: Text('로그인'),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    primary: Colors.black,
+                  ),
+                  child: Text('회원가입'),
+                ),
+              ],
+            ),
           ),
-          CustomTextFormField(
-            hintText: '비밀번호를 입력해주세요.',
-            onChanged: (String value) {},
-            obscureText: true,
-          ),
-        ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Title extends StatelessWidget {
+  const _Title({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text('환영합니다!',
+        style: TextStyle(
+          fontSize: 34,
+          fontWeight: FontWeight.w500,
+          color: Colors.black,
+        ));
+  }
+}
+
+class _SubTitle extends StatelessWidget {
+  const _SubTitle({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '이메일과 비밀번호를 입력해서 로그인 해주세요!\n오늘도 성공적인 주문이 되길 :)',
+      style: TextStyle(
+        fontSize: 16,
+        color: BODY_TEXT_COLOR,
       ),
     );
   }
