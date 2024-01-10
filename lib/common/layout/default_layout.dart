c687e386eedf1b4d57adbccb2de4ae7c46fda489
tlgusdl03 <tlgusdl03@gmail.com>
2024-01-10 23:42:26 +0900

    appbar와 bottomNavigationBar 추가

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -3,21 +3,49 @@ import 'package:flutter/material.dart';
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
+  final Widget? bottomNavigationBar;
 
-  const DefaultLayout({
-    required this.child,
-    this.backgroundColor,
-    super.key});
+  const DefaultLayout(
+      {required this.child,
+      this.backgroundColor,
+      this.title,
+      this.bottomNavigationBar,
+      super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       //입력받은 색상이 없다면 흰색
+      //앱바와 바텀네비게이션바 추가
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppBar(),
       body: child,
+      bottomNavigationBar: bottomNavigationBar,
     );
   }
+
+  AppBar? renderAppBar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text(
+          //절대로 null이 될 수 없음을 나타냄
+          title!,
+          style: TextStyle(
+            fontSize: 16.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
+  }
 }
+
 class hello extends StatelessWidget {
   const hello({super.key});
 
@@ -26,4 +54,3 @@ class hello extends StatelessWidget {
     return Container();
   }
 }
-
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,15 +1,49 @@
 import 'package:flutter/material.dart';
+import 'package:inputform_proj/common/const/colors.dart';
 import 'package:inputform_proj/common/layout/default_layout.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
 
   @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
+  @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      title: '코팩 딜리버리',
       child: Center(
         child: Text('RootTab'),
       ),
+      //default_layout.dart에 있는 bottomNavigationBar를 사용
+      //BottomNavigationBar에 있는 속성들을 이용함
+      //onTap과 currentIndex를 사용하려면 statefulwidget을 사용해야 함
+      //onTap과 currentIndex를 사용하여 선택한 아이템을 바꿀 수 있음
+      bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: PRIMARY_COLOR,
+          unselectedItemColor: BODY_TEXT_COLOR,
+          selectedFontSize: 10,
+          unselectedFontSize: 10,
+          type: BottomNavigationBarType.fixed,
+          onTap: (int index) {
+            setState(() {
+              this.index = index;
+            });
+          },
+          currentIndex: index,
+          items: [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: '홈'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.fastfood_outlined), label: '음식'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.receipt_long_outlined), label: '주문'),
+            BottomNavigationBarItem(
+                icon: Icon(Icons.person_outline), label: '프로필'),
+          ]),
     );
   }
 }
